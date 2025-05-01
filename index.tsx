b810a8d6ac45f16630630bb9b02dfd8354181985
marshi07 <marshikons@gmail.com>
2025-05-02 03:17:10 +0530

    Delete index.tsx

--- index.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ height: "100vh", background: "#111", color: "#fff", display: "flex", alignItems: "center", justifyContent: "center", fontSize: "2rem" }}>
-      Hello from PromptClips!
-    </div>
-  );
-}
