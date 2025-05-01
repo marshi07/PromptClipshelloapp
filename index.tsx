672c99d6c1e38fa849d997ccba346d66a95b493c
marshi07 <marshikons@gmail.com>
2025-05-02 03:18:26 +0530

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <div style={{
+      height: "100vh",
+      background: "#111",
+      color: "#fff",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "2rem"
+    }}>
+      Hello from PromptClips!
+    </div>
+  );
+}
